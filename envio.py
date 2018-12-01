7718e0ce085016456e9b33cec37e7d19ee031bb3
pabmunlop <pabmunlop@gmail.com>
2018-12-01 11:49:22 +0100

    Merge branch 'master' of https://github.com/D3algr4t/UPOFURGO

--- envio.py
+++ envio.py
@@ -1,4 +1,3 @@
-
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
@@ -20,7 +19,6 @@
 #    along with this program.  If not, see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-
 
 from osv import osv
 from osv import fields
